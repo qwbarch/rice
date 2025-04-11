7b28acc15104e2a86ac6ff0d367130881143bc09
Edward Yang <qwbarch@gmail.com>
2025-04-11 18:05:14 -0400

    Add initial installer

---
+++ install.py
@@ -0,0 +1,12 @@
+import subprocess
+import shutil
+
+if shutil.which("scoop") is None:
+    print("Installing scoop...")
+    command = """
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+    """
+    subprocess.run(["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command", command], check=True)
+else:
+    print("Found scoop, skipping installation.")
